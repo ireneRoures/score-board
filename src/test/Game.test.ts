478bd5d44fa2194bb36cd3f3a02caede769ea07a
ireneR <irenerouresvillalonga@gmail.com>
2021-01-20 12:14:01 +0100

    More tests testing finish game

--- src/test/Game.test.ts
+++ src/test/Game.test.ts
@@ -34,3 +34,16 @@ test('finish a game', () => {
     gameService.finish(gameId.id)
     expect(gameService.get().length).toBe(0)
 })
+
+test('finish a game that do not exist', () => {
+    const gameService = new GameService()
+    const gameId = gameService.add('local team', 'away team')
+    gameService.finish(123)
+    expect(gameService.get().length).toBe(1)
+})
+
+test('finish a game without started games', () => {
+    const gameService = new GameService()
+    gameService.finish(123)
+    expect(gameService.get().length).toBe(0)
+})

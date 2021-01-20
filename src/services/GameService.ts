ffb136f48db61de1cc2987b856fac7f02b097e87
ireneR <irenerouresvillalonga@gmail.com>
2021-01-20 10:37:49 +0100

    Format code

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -4,6 +4,7 @@ export class GameService {
     get() {
         return [];
     }
+    
     add(localTeam: string, awayTeam: string) {
         return new Game(localTeam, awayTeam)
     }
--- src/test/Game.test.ts
+++ src/test/Game.test.ts
@@ -13,11 +13,11 @@ test('get list of games empty', () => {
 
 test('create game', () => {
     const gameService = new GameService()
-    const game = gameService.add('local team', 'away team');
+    const game = gameService.add('local team', 'away team')
 
-    expect(game.local.name).toBe('local team');
-    expect(game.away.name).toBe('away team');
+    expect(game.local.name).toBe('local team')
+    expect(game.away.name).toBe('away team')
 
-    expect(game.localScore).toBe(0);
-    expect(game.awayScore).toBe(0);
+    expect(game.localScore).toBe(0)
+    expect(game.awayScore).toBe(0)
 })
